fea25b6df7d33fa4518d4a0cca7141a340c6f552
Christopher McDonald <chrismcband@gmail.com>
2018-05-08 02:31:27 +0100

    Added script to start gunicorn.

---
+++ config/gunicorn.py
@@ -0,0 +1,26 @@
+bind = "127.0.0.1:8000"
+backlog = 256
+workers = 4
+worker_class = 'gevent'
+worker_connections = 256
+max_requests =  512
+timeout = 90
+keepalive = 1
+
+proc_name = 'gunicorn'
+default_proc_name = 'gunicorn'
+pidfile = '/tmp/emailreplydemo.gunicorn.pid'
+
+preload_app = False
+daemon = False
+
+debug = False
+spew = False
+
+umask = 0
+tmp_upload_dir = None
+
+logfile = '/tmp/emailreplydemo.gunicorn.log'
+errorlog = logfile
+loglevel = 'warning'
+logconfig = None
---
+++ utility/gunicorn_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DEFAULT_CODE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
+CODE_ROOT=${CODE_ROOT:=$DEFAULT_CODE_ROOT}
+DEFAULT_VENV="/home/$(whoami)/.virtualenvs/email_reply_demo"
+VENV=${VENV:=$DEFAULT_VENV}
+
+cd $CODE_ROOT/src
+exec ${VENV}/bin/gunicorn -c $CODE_ROOT/config/gunicorn.py config.wsgi
