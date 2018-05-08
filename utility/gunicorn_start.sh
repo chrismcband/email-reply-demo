6c712ac90f5353facc4a7951986c821e509725ea
Christopher McDonald <chrismcband@gmail.com>
2018-05-08 02:34:32 +0100

    Attempt to fix issue in gunicorn script.

--- utility/gunicorn_start.sh
+++ utility/gunicorn_start.sh
@@ -5,5 +5,5 @@ CODE_ROOT=${CODE_ROOT:=$DEFAULT_CODE_ROOT}
 DEFAULT_VENV="/home/$(whoami)/.virtualenvs/email_reply_demo"
 VENV=${VENV:=$DEFAULT_VENV}
 
-cd $CODE_ROOT/src
+cd $CODE_ROOT
 exec ${VENV}/bin/gunicorn -c $CODE_ROOT/config/gunicorn.py config.wsgi
